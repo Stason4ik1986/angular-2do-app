4bf37f2e12327577ddec1a2a59d44ed5325b14a0
Stas Cherednichenko <first1986st@gmail.com>
2017-10-13 10:59:02 +0300

    Create Todo App with rxjs observable

--- src/app/lessons-counter/lessons-counter.component.ts
+++ src/app/lessons-counter/lessons-counter.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observer } from 'rxjs/Observer';
+
 import { Lesson } from '../shared/model/lesson';
-import { store, Observer } from '../events-bus-experiments/app-data';
+import { store } from '../events-bus-experiments/app-data';
 
 @Component({
   selector: 'lessons-counter',
   templateUrl: './lessons-counter.component.html',
   styleUrls: ['./lessons-counter.component.css']
 })
-export class LessonsCounterComponent implements Observer, OnInit {
+export class LessonsCounterComponent implements Observer<Lesson[]>, OnInit {
+
   lessonsCounter = 0;
 
   ngOnInit() {
-    store.subscribe(this);
+    store.lessonsList$.subscribe(this);
   }
 
   next(data: Lesson[]) {
     this.lessonsCounter = data.length;
   }
 
+  error(err: any) {
+    console.log(err);
+  }
+
+  complete() {
+    console.log('Completed');
+  }
 }
